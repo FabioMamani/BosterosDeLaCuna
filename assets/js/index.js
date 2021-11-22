d924182cd60e2b0ffba89bbda88a12f80f917c0a
fabiomamani <fabiomamaniunm@gmail.com>
2021-11-22 18:41:03 -0300

    Se termino de maquetar el sitio, se creo la interfaz para las cards y el footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,33 @@ buttonMain.innerHTML = `<h3>Button</h3>`
 mainContain.appendChild(containTitular)
 mainContain.appendChild(containCarrusel)
 mainContain.appendChild(buttonMain)
+
+/*##### SECCION CONTENIDO DE LA PAGE ########*/
+const containSecond = document.querySelector('.contain-second');
+const containCard = document.createElement('div')
+containCard.classList.add('contain-card');
+const containText = document.createElement('div')
+containText.classList.add('contain-text');
+
+const containCard2 = document.createElement('div')
+containCard2.classList.add('contain-card2');
+const containText2 = document.createElement('div')
+containText2.classList.add('contain-text2');
+
+const containCard3 = document.createElement('div')
+containCard3.classList.add('contain-card3');
+const containText3 = document.createElement('div')
+containText3.classList.add('contain-text3');
+
+containSecond.appendChild(containCard)
+containSecond.appendChild(containText)
+containSecond.appendChild(containCard2)
+containSecond.appendChild(containText2)
+containSecond.appendChild(containCard3)
+containSecond.appendChild(containText3)
+
+const containCopas = document.querySelector('.contain-copas');
+const titularCopas = document.createElement('div')
+titularCopas.classList.add('titular-copas')
+titularCopas.innerHTML = `<h2>Titular de Copas</h2>`;
+containCopas.appendChild(titularCopas)
